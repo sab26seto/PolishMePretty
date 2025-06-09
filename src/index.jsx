1200ae67ce0df19ec240755705a081524c14a881
sab26seto <setosabrina26@gmail.com>
2025-06-09 11:49:55 -0400

    comit

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'react-datepicker/dist/react-datepicker.css';
+import {BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename="/PolishMePretty">
     <App />
+    </BrowserRouter>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
